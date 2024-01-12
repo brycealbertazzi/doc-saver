482d396777ba6d4c71625c739e182076af4eff4d
brycealbertazzi <brycealbertazzi@gmail.com>
2024-01-12 15:05:43 -0700

    Init project

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="home">
+    <Navbar/>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import Navbar from './Navbar.vue'
+export default {
+  name: 'HomePage',
+  props: {
+    welcomeMsg: String
+  },
+  components: {
+    Navbar,
+    Navbar
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="navbar">
+        <h1 class="app-title">Doc Saver</h1>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style scoped>
+.navbar {
+    background-color: #37be81;
+    color: #fff;
+    margin-bottom: 20px;
+    width: 100%;
+}
+.app-title {
+    margin: 0;
+    font-size: 1.5em;
+    font-weight: 400;
+}
+</style>
